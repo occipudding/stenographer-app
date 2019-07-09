695ee32a97c8dcbc3bd8b2d58458ca68b62b5bdd
github username <github email address>
2019-07-09 13:56:29 -0400

    add id to controllers

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,6 @@
 class TopicsController < ApplicationController
   def index
     topics = Topic.all
-    render json: topics.as_json(only: [:title, :tags, :user])
+    render json: topics.as_json(only: [:id, :title, :tags, :user])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
     users = User.all
-    render json: users.as_json( only: [:name, :topics])
+    render json: users.as_json(only: [:id, :name, :topics])
   end
 end
